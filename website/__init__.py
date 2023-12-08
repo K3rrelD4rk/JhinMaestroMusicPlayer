7e0f301206b39a661ed37624d341fcf6f3995d50
K3rrel <61194005+K3rrelD4rk@users.noreply.github.com>
2023-12-08 15:28:11 +0100

    Update 08/12/2023
    - Added app creation
    - Added database creation
    - Added program for login on Spotify

---
+++ website/__init__.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+import random, string
+from APIs import auth
+
+db = SQLAlchemy()
+DB_NAME = "offline.db"
+
+
+def get_random_password():
+    random_source = string.ascii_letters + string.digits + string.punctuation
+    # select 1 lowercase
+    password = random.choice(string.ascii_lowercase)
+    # select 1 uppercase
+    password += random.choice(string.ascii_uppercase)
+    # select 1 digit
+    password += random.choice(string.digits)
+    # select 1 special symbol
+    password += random.choice(string.punctuation)
+
+    # generate other characters
+    for i in range(6):
+        password += random.choice(random_source)
+
+    password_list = list(password)
+    # shuffle all characters
+    random.SystemRandom().shuffle(password_list)
+    password = ''.join(password_list)
+    return password
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from APIs import auth
+    app.register_blueprint(auth, url_prefix='/')
+    from database import User
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created database')
