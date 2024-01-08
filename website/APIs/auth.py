712d32c4f0f58ee4b882b1669c4295416ce55443
K3rrel <61194005+K3rrelD4rk@users.noreply.github.com>
2024-01-08 10:46:14 +0100

    inizio remake auth API

--- website/APIs/auth.py
+++ website/APIs/auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import requests
 
-LOGIN_URL = "https://accounts.spotify.com/api/login"
-
-CLIENT_ID = "be5f21610a7347d08a5fc9c621b7f141"
-SECRET_CLIENT = "109ba1df69804424a5ff820522549cfa"
+export LOGIN_URL = "https://accounts.spotify.com/api/login"
+export CLIENT_ID = "be5f21610a7347d08a5fc9c621b7f141"
+export SECRET_CLIENT = "109ba1df69804424a5ff820522549cfa"
+export REDIRECT_URL = "https://localhost:3000"
