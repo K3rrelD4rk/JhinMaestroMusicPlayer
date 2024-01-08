0c4efce7254ebf806527a96c2351db5c4d9f8008
K3rrel <61194005+K3rrelD4rk@users.noreply.github.com>
2024-01-08 21:40:00 +0100

    Update auth.py

--- website/APIs/auth.py
+++ website/APIs/auth.py
@@ -9,3 +9,8 @@ LOGIN_URL = "https://accounts.spotify.com/api/login"
 CLIENT_ID = "be5f21610a7347d08a5fc9c621b7f141"
 SECRET_CLIENT = "109ba1df69804424a5ff820522549cfa"
 REDIRECT_URI = "https://localhost:3000"
+results = sp.current_user_saved_tracks()
+
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artist'][0]['name'], " - ", track['name'])
