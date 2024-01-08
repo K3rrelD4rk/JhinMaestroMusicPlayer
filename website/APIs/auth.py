efe9c3b9fbbdee5d9841e2bf6921c9c5425ecc5f
K3rrel <61194005+K3rrelD4rk@users.noreply.github.com>
2024-01-08 12:37:12 +0100

    updated auth.py a bit

--- website/APIs/auth.py
+++ website/APIs/auth.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import requests
+#import requests
 
-export LOGIN_URL = "https://accounts.spotify.com/api/login"
-export CLIENT_ID = "be5f21610a7347d08a5fc9c621b7f141"
-export SECRET_CLIENT = "109ba1df69804424a5ff820522549cfa"
-export REDIRECT_URL = "https://localhost:3000"
+scope = "user-library-read"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+LOGIN_URL = "https://accounts.spotify.com/api/login"
+CLIENT_ID = "be5f21610a7347d08a5fc9c621b7f141"
+SECRET_CLIENT = "109ba1df69804424a5ff820522549cfa"
+REDIRECT_URI = "https://localhost:3000"
