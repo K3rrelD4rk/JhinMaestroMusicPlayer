d92064fac048acba8dee6b076309e0bf84137a74
K3rrel <61194005+K3rrelD4rk@users.noreply.github.com>
2024-02-11 19:25:25 +0100

    access token ottenuto

--- website/APIs/auth.py
+++ website/APIs/auth.py
@@ -9,3 +9,5 @@ SECRET_CLIENT = "109ba1df69804424a5ff820522549cfa"
 h = {"Content-Type" : "application/x-www-form-urlencoded"}
 d = {'grant_type' : 'client_credentials' }
 p = s.post( url , headers = h , data = d, auth = (CLIENT_ID , SECRET_CLIENT))
+a = json.loads(p.content.decode("utf-8"))
+token = a["access_token"]
