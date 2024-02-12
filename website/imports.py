216379bdf5080d596f5bef562a01dd86f8c6b58c
K3rrel <61194005+K3rrelD4rk@users.noreply.github.com>
2024-02-12 09:53:44 +0100

    Update imports.py

--- website/imports.py
+++ website/imports.py
@@ -1,21 +1,23 @@
 import random
+from dotenv import load_dotenv
 import requests
 import json
 from io import StringIO
 import os
 import hashlib
+load_dotenv()
 
+s = requests.session()
 def verify(client_id, redirect_uri):
     code_verify = hashlib.sha256.digest(random.Random(128))
     scope = "user-read-private user-read-email"
     auth_url = "https://accounts.spotify.com/authorize"
-    s = requests.session()
-    g = s.get(auth_url, auth =(client_id), data = { "scope" : scope, "response_type" : 'code', "code_challenge" : code_verify, "code_challenge_method" : 'S256'})
-
+    h = {"response_type" : 'code'}
+    g = s.get(auth_url, headers = h ,auth =(client_id), data = { "scope" : scope, "code_challenge" : code_verify, "code_challenge_method" : 'S256'})
+    c = json.loads(g.content.decode('utf-8'))
 
 def token(client_id, client_secret, redirect_uri):
     url = "https://accounts.spotify.com/api/token"
-    s = requests.session()
     h = {"Content-Type": "application/x-www-form-urlencoded"}
     d = {'grant_type': 'client_credentials'}
     p = s.post(url, headers=h, data=d, auth=(client_id, client_secret))
@@ -26,11 +28,15 @@ def token(client_id, client_secret, redirect_uri):
 
 #def get_profile(userid, email):
 client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+client_secret = os.getenv("SECRET_CLIENT")
 
-code_verify = hashlib.sha256.digest(random.Random(128))
+#code_verify = hashlib.sha256.digest(random.Random(128))
+code_verify = hashlib.sha256(hashlib.digest(random.Random(128)))
 scope = "user-read-private user-read-email"
 auth_url = "https://accounts.spotify.com/authorize"
+h = {"response_type" : 'code'}
+d = { "scope" : scope, "code_challenge" : code_verify, "code_challenge_method" : 'S256'}
 s = requests.session()
-g = s.get(auth_url, data = { "scope" : scope, "response_type" : 'code', "code_challenge" : code_verify, "code_challenge_method" : 'S256'},)
-print()
+g = s.get(auth_url, headers = h , data = d)
+c = json.loads(g.content.decode('utf-8'))
+print(c)
